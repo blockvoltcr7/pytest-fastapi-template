9a6efef957301ab88d70b8e447ed36b3203e2e51
Sami Sabir-Idrissi <sami@ssiautomations.com>
2025-06-14 11:46:15 -0400

    add token auth

--- app/api/v1/api.py
+++ app/api/v1/api.py
@@ -2,8 +2,12 @@ from fastapi import APIRouter
 from app.api.v1.endpoints.hello_world_v1 import router as hello_world_router
 from app.api.v1.endpoints.crewai_v1 import router as crewai_router
 from app.api.v1.endpoints.crewai_v1_content_crew import router as content_crew_router
+from app.api.v1.endpoints.auth import router as auth_router
+from app.api.v1.endpoints.users import router as users_router
 
 api_router = APIRouter()
 api_router.include_router(hello_world_router, tags=["hello world"])
 api_router.include_router(crewai_router, tags=["crewai"])
 api_router.include_router(content_crew_router, tags=["content crew"])
+api_router.include_router(auth_router, tags=["authentication"])
+api_router.include_router(users_router, tags=["users"])
